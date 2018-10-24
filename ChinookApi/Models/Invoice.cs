e99789a4c04129af0df7478cabce993a13542ffb
Barry Russell <blrussell09@gmail.com>
2018-10-23 19:38:04 -0500

    Created endpoint that will return a list of invoices belonging to the provided sales agent

---
+++ ChinookApi/Models/Invoice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChinookApi.Models
+{
+  public class Invoice
+  {
+
+    public int Id { get; set;}
+    public DateTime InvoiceDate { get; set;}
+    public string BillingAddress {get; set;}
+    public string BillingCity {get; set;}
+    public string BillingState {get; set;}
+    public string BillingCountry {get; set;}
+    public string BillingPostalCode {get; set;}
+    public decimal Total {get; set;}
+    public string Agent_Name { get; set; }
+    public string Customer_Name { get; set; }
+  }
+}
