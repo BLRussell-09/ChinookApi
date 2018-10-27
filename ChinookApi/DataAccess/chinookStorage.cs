a43b12f54148a3359a8e4f7306039f6f7cf32cd9
Barry Russell <blrussell09@gmail.com>
2018-10-27 11:49:10 -0500

    Added dapper, switched out the sqlquery for a dapper one on the endpoint that returns invoices by employee id

--- ChinookApi/DataAccess/chinookStorage.cs
+++ ChinookApi/DataAccess/chinookStorage.cs
@@ -1,4 +1,6 @@
 ﻿using ChinookApi.Models;
+using Dapper;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -10,7 +12,12 @@ namespace ChinookApi.DataAccess
   public class ChinookStorage
   {
 
-    private const string conString = "Server=(local);Database=Chinook;Trusted_Connection=True;";
+    private readonly string conString;
+
+    public ChinookStorage(IConfiguration config)
+    {
+      conString = config.GetSection("ConnectionString").Value;
+    }
 
     public List<InvoiceAll> GetAllInvoices()
     {
@@ -43,41 +50,51 @@ namespace ChinookApi.DataAccess
       }
     }
 
-    public List<Invoice> GetbyId(int id)
+    public IEnumerable<Invoice> GetbyId(int id)
     {
       using (var connection = new SqlConnection(conString))
       {
         connection.Open();
-        var command = connection.CreateCommand();
-        command.CommandText = @"select Agent_Name = e.FirstName + ' ' + e.LastName, Customer_Name = c.FirstName + ' ' + c.LastName, *
+
+        var result = connection.Query<Invoice>(@"select Agent_Name = e.FirstName + ' ' + e.LastName, Customer_Name = c.FirstName + ' ' + c.LastName, *
                                 from Invoice as i, Customer as c, Employee as e
                                 where i.CustomerId = c.CustomerId
                                 and e.EmployeeId = c.SupportRepId
                                 and c.SupportRepId = @id
-                                order by EmployeeId";
+                                order by EmployeeId", new {id = id });
 
-        command.Parameters.AddWithValue("@id", id);
 
-        var reader = command.ExecuteReader();
-        var invoiceHolder = new List<Invoice>();
-        while (reader.Read())
-        {
-          var invoice = new Invoice()
-          {
-            Id = (int)reader["InvoiceId"],
-            BillingAddress = reader["BillingAddress"].ToString(),
-            BillingCity = reader["BillingCity"].ToString(),
-            BillingCountry = reader["BillingCountry"].ToString(),
-            BillingPostalCode = reader["BillingPostalCode"].ToString(),
-            BillingState = reader["BillingState"].ToString(),
-            InvoiceDate = DateTime.Parse(reader["InvoiceDate"].ToString()),
-            Agent_Name = reader["Agent_Name"].ToString(),
-            Customer_Name = reader["Customer_Name"].ToString(),
-            Total = (decimal)reader["Total"],
-          };
-          invoiceHolder.Add(invoice);
-        }
-        return invoiceHolder;
+
+        //var command = connection.CreateCommand();
+        //command.CommandText = @"select Agent_Name = e.FirstName + ' ' + e.LastName, Customer_Name = c.FirstName + ' ' + c.LastName, *
+        //                        from Invoice as i, Customer as c, Employee as e
+        //                        where i.CustomerId = c.CustomerId
+        //                        and e.EmployeeId = c.SupportRepId
+        //                        and c.SupportRepId = @id
+        //                        order by EmployeeId";
+
+        //command.Parameters.AddWithValue("@id", id);
+
+        //var reader = command.ExecuteReader();
+        //var invoiceHolder = new List<Invoice>();
+        //while (reader.Read())
+        //{
+        //  var invoice = new Invoice()
+        //  {
+        //    Id = (int)reader["InvoiceId"],
+        //    BillingAddress = reader["BillingAddress"].ToString(),
+        //    BillingCity = reader["BillingCity"].ToString(),
+        //    BillingCountry = reader["BillingCountry"].ToString(),
+        //    BillingPostalCode = reader["BillingPostalCode"].ToString(),
+        //    BillingState = reader["BillingState"].ToString(),
+        //    InvoiceDate = DateTime.Parse(reader["InvoiceDate"].ToString()),
+        //    Agent_Name = reader["Agent_Name"].ToString(),
+        //    Customer_Name = reader["Customer_Name"].ToString(),
+        //    Total = (decimal)reader["Total"],
+        //  };
+        //  invoiceHolder.Add(invoice);
+        //}
+        return result;
 
       };
 
--- ChinookApi/Startup.cs
+++ ChinookApi/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ChinookApi.DataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,8 @@ namespace ChinookApi
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+      services.AddSingleton<IConfiguration>(Configuration);
+      services.AddScoped<ChinookStorage>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
